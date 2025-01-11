d5e62754372bd70371e020bbfabb0d6b9660d771
João Fillipe <joaofillipe.ben.assis@gmail.com>
2025-01-11 09:03:11 -0300

    model Pagamento

--- models/models.py
+++ models/models.py
@@ -1,11 +1,18 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 from datetime import date
 from decimal import Decimal
 
 class Assinatura(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    id: int = Field(primary_key=True)
     empresa: str
     site: Optional[str] = None
     data_assinatura: date
     valor: Decimal
+
+class Pagamento(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    # assinatura.id = campo id da classe Assinatura
+    assinatura_id: int = Field(foreign_key='assinatura.id')
+    assinatura: Assinatura = Relationship()
+    data_pagamento: date
